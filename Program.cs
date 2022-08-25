85b5f9d8b651834069da9edaa1d613eeda516ab4
Endergreen12 <endergreenjapan@gmail.com>
2022-08-26 01:50:14 +0900

    英語から日本語に変更 ダンプするファイルの数と残り何パーセントの表示を追加

--- Program.cs
+++ Program.cs
@@ -1,33 +1,50 @@
 ﻿using Microsoft.Data.Sqlite;
+using System.Reflection.PortableExecutable;
+using System.Threading;
 
-// Start up
-Console.WriteLine("Umamusume Assets Extractor");
+var uaeName = "Umamusume Assets Extractor";
 
-// Set extract folder name
-var extractFolderName = "Contents";
-Console.Write("\r\nEnter the name of the folder where the extracted content will be stored.\r\nWarning: If entered the name of folder already exists, it will be deleted.\r\nType the folder name:");
-extractFolderName = Console.ReadLine();
+// 起動
+Console.Title = uaeName;
+Console.WriteLine(uaeName);
+Console.WriteLine("自分が見たいファイルがなかった場合はウマ娘で一括ダウンロードをしてください。");
 
-// Specify the folder to dump if necessary
+// フォルダー名の指定
+var extractFolderName = "";
+while (extractFolderName == "")
+{
+    Console.Write("\r\n展開したファイルを保存するフォルダー名を入力してください。\r\nもしその名前のフォルダーが既に存在していた場合、そのフォルダーは削除されます。\r\nフォルダー名を入力:");
+    extractFolderName = Console.ReadLine();
+
+    if (extractFolderName != "")
+        break;
+
+    Console.WriteLine("空欄は許可されていません。もう一度入力してください。");
+}
+
+// 必要な場合展開するフォルダーを指定する(ウマ娘側のフォルダーという意味)
 var dumpFolder = "";
-Console.Write("\r\nEnter the name of the folder you want to dump if you need. (Example: story, live, sound etc.)\r\nIf you don't need, please leave blank.\r\nType the folder name:");
+Console.Write("\r\n必要な場合、ダンプするウマ娘のアセットのフォルダーを指定してください。(例: story, live, sound など)\r\n空欄にするとすべてをダンプしますが、とんでもない時間がかかるのでダンプしたいものを指定することをお勧めします。\r\nフォルダー名を入力:");
 dumpFolder = Console.ReadLine();
 
-Console.Write("\r\n"); // Since the line is not broken, break the line here
-
-// If already the folder exists, will be deleted and recreated.
+// フォルダーが存在していた場合、ここで削除
 if (Directory.Exists(extractFolderName))
+{
+    Console.WriteLine($"フォルダー{extractFolderName}を削除中");
     Directory.Delete(extractFolderName, true);
+}
 
+Console.WriteLine($"フォルダー{extractFolderName}を作成中");
 Directory.CreateDirectory(extractFolderName);
 
-// Variable Setup
+// 変数の宣言
 var localappdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
 var gamePath = localappdata + @"Low\Cygames\umamusume";
 var metaPath = gamePath + @"\meta";
 var datPath = gamePath + @"\dat";
+var willBeCopiedFilesAmount = 0;
 
-// File Copy Process
+// ファイルをコピー
 if (File.Exists(metaPath))
 {
     using (var connection = new SqliteConnection($"Data Source={metaPath}"))
@@ -35,56 +52,92 @@ if (File.Exists(metaPath))
         connection.Open();
 
         var command = connection.CreateCommand();
-        command.CommandText = // n is file directory, h is source file name.
+        command.CommandText = // nはディレクトリ、 hはソースファイル
         @"
             SELECT n, h
             FROM a
-        ";
+        "
+        ;
+
+        Console.WriteLine("ファイルのコピーを開始しました。(もしこの画面で長い時間止まっている場合は、ダンプするフォルダーの名前を存在しないものに指定している可能性があります。その場合、結局何もコピーされないので、ただ待たされるだけです。)");
 
         using (var reader = command.ExecuteReader())
         {
+            Thread thread = new Thread(new ThreadStart(CalculateWillBeCopiedFilesAmount)); // 裏でコピーするファイル数の計算
+            thread.Start();
+
             var copiedFileAmount = 0;
             while (reader.Read())
             {
-                var fileDir = reader.GetString(0).Split("/"); // The file directory is stored like "sound/v/snd_voi_race_104602.acb", so we will separate this at "/"
-                var sourceFileName = reader.GetString(1);
-                var sourceDir = datPath + @"\" + sourceFileName.Substring(0, 2) + @"\" + sourceFileName; // See note 1 at the bottom
+                var fileDir = reader.GetString(0).Split("/"); // 例:"sound/v/snd_voi_race_104602.acb"
+                var sourceFileName = reader.GetString(1);     // 例:"EK5FIH4TY23JRVW2XNTCNCEQGSZSRFPT"
+                var sourceDir = datPath + @"\" + sourceFileName.Substring(0, 2) + @"\" + sourceFileName; // 例:"datPath\EK\EK5FIH4TY23JRVW2XNTCNCEQGSZSRFPT"
 
-                var shouldBeSkipped = false;
-                if (dumpFolder != "" && fileDir[0] != dumpFolder || !File.Exists(sourceDir) || reader.GetString(0).Substring(0, 2) == "//")
-                    shouldBeSkipped = true;     // If any other folders are attempted to be dumped, they are skipped if specified dump folder
-                                                // Also, if the source file doesn't download, it will be skipped. And manifest file will be skipped too. idk what this is
-                if (shouldBeSkipped)
+                if (!CheckCanBeCopied(reader, datPath, dumpFolder, fileDir, sourceFileName, sourceDir))
                     continue;
 
-                Console.WriteLine($"Copying {sourceFileName} to {String.Join(@"\", fileDir)}...");
+                Console.WriteLine($"{sourceFileName} -> {String.Join(@"\", fileDir)}");
 
-                var extractDir = Directory.CreateDirectory(extractFolderName + @"\" + String.Join(@"\", fileDir.SkipLast(1))); // See note 2
+                var extractDir = Directory.CreateDirectory(extractFolderName + @"\" + String.Join(@"\", fileDir.SkipLast(1))); // ディレクトリを作成してパスをextractDirに格納
 
-                File.Copy(sourceDir, extractDir + @"\" + fileDir.Last()); // Copying the source file to extract folder
+                File.Copy(sourceDir, extractDir + @"\" + fileDir.Last()); // ソースファイルを保存先にコピー
 
                 copiedFileAmount++;
+
+                int donePer = (int)((float)copiedFileAmount / (float)willBeCopiedFilesAmount * 100);
+
+                Console.Title = $"{uaeName} - 残り {copiedFileAmount} / {willBeCopiedFilesAmount} - {donePer}%完了";
             }
 
-            Console.WriteLine($"Done. Total copied file:{copiedFileAmount}");
+            Console.WriteLine($"完了しました。 トータルでコピーしたファイル: {copiedFileAmount}");
         }
     }
 } else
 {
-    Console.WriteLine("Metafile not found.\r\nIs Umamusume installed?");
+    Console.WriteLine("metaファイルが見つかりませんでした。\r\n一回ウマ娘を起動してみてください。");
 }
 
-Console.WriteLine("Press any key to continue...");
-Console.ReadKey();
+Console.WriteLine("展開したフォルダーをエクスプローラーで開きますか？(Y)");
+var pressedKey = Console.ReadKey();
+if (pressedKey.Key == ConsoleKey.Y)
+    System.Diagnostics.Process.Start("explorer.exe", dumpFolder);
 
-/*Note 1
- For example, source file "2A2CW2FWSLHH5SZYPI2OTJXA3SFH234W" is stored at "dat\2A\2A2CW2FWSLHH5SZYPI2OTJXA3SFH234W".
- The name of the directory after dat is the first two letters of the source file name.
- So, using Substring() to get first two letters and connect both.
- */
+void CalculateWillBeCopiedFilesAmount()
+{
+    using (var connection = new SqliteConnection($"Data Source={metaPath}"))
+    {
+        connection.Open();
 
-/*Note 2
- This is creating directory to be copied the source file.
- splited file name is like "sound", "v" and "snd_voi_race_104602.acb".
- To create directory, we don't need the file name, so using SkipLast() to skip it and store to extractDir.
- */
+        var command = connection.CreateCommand();
+        command.CommandText =
+        @"
+            SELECT n, h
+            FROM a
+        "
+        ;
+
+        using (var reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                var fileDir = reader.GetString(0).Split("/");
+                var sourceFileName = reader.GetString(1);
+                var sourceDir = datPath + @"\" + sourceFileName.Substring(0, 2) + @"\" + sourceFileName;
+
+                if (!CheckCanBeCopied(reader, datPath, dumpFolder, fileDir, sourceFileName, sourceDir))
+                    continue;
+
+                willBeCopiedFilesAmount++;
+            }
+        }
+
+    }
+}
+
+bool CheckCanBeCopied(SqliteDataReader reader, string datPath, string dumpFolder, string[] fileDir, string sourceFileName, string sourceDir)
+{
+    if (dumpFolder != "" && fileDir[0] != dumpFolder || !File.Exists(sourceDir) || reader.GetString(0).Substring(0, 2) == "//")
+        return false;     // ダンプするフォルダーが指定されていて、ダンプしようとしたフォルダーがそれではない場合と、ソースファイルがまだダウンロードされていなかった場合、
+                          // ファイル名の最初に"//"がついてるファイル(manifest?)というファイルだった場合はコピーをスキップします。
+    return true;
+}
